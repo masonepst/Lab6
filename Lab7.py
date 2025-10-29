c03223964daab029987e4d12d5e911f5b089aa3f
masonepst <masonepst@gmail.com>
2025-10-28 21:20:05 -0400

    Add files via upload

--- Lab7.py
+++ Lab7.py
@@ -1,8 +1,19 @@
 import socket
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+pins = [23,24,25]
 
 led = [0,0,0]
 select = 0
+bright = []
+
+for i in pins:
+	GPIO.setup(i, GPIO.OUT)
+	pwm = GPIO.PWM(i, 100)
+	pwm.start(0)
+	bright.append(pwm)
+
 
 s = socket.socket()
 
@@ -20,36 +31,45 @@ def parsePOSTdata(data):
 		if len(key_val) == 2:
 			data_dict[key_val[0]] = key_val[1]
 	return data_dict
+try:
+	while True:
 
-while True:
+		conn, addr = s.accept()
+		request = conn.recv(1024)
 
-	conn, addr = s.accept()
-	request = conn.recv(1024)
+		data = parsePOSTdata(request.decode())
 
-	data = parsePOSTdata(request).decode()
-
-	if 'led' in data and 'brightness' in data:
-		select = int(data['led'])
-		led[select] = int(data['brightness'])
+		if 'led' in data and 'brightness' in data:
+			select = int(data['led'])
+			led[select] = int(data['brightness'])
+			bright[select].ChangeDutyCycle(led[select])
 
 
-	html = f"""<!DOCTYPE html>
-<html>
-<body>
-  <h2>Brightness level:</h2>
-  <form method="POST">
-    <input type="range" name="brightness" min="0" max="100" value="{led[select]}"><br><br>
-    <b>Select LED:</b><br>
-    <input type="radio" name="led" value="0" {'checked' if select == 0 else ''}> LED 1 ({led[0]}%)<br>
-    <input type="radio" name="led" value="1" {'checked' if select == 1 else ''}> LED 2 ({led[1]}%)<br>
-    <input type="radio" name="led" value="2" {'checked' if select == 2 else ''}> LED 3 ({led[2]}%)<br><br>
-    <input type="submit" value="Change Brightness">
-  </form>
-</body>
-</html>"""
+		html = f"""<!DOCTYPE html>
+	<html>
+	<body>
+	  <h2>Brightness level:</h2>
+	  <form method="POST">
+	    <input type="range" name="brightness" min="0" max="100" value="{led[select]}"><br><br>
+	    <b>Select LED:</b><br>
+	    <input type="radio" name="led" value="0" {'checked' if select == 0 else ''}> LED 1 ({led[0]}%)<br>
+	    <input type="radio" name="led" value="1" {'checked' if select == 1 else ''}> LED 2 ({led[1]}%)<br>
+	    <input type="radio" name="led" value="2" {'checked' if select == 2 else ''}> LED 3 ({led[2]}%)<br><br>
+	    <input type="submit" value="Change Brightness">
+	  </form>
+	</body>
+	</html>"""
 
-	conn.send(b"HTTP/1.1 200 OK\r\n")
-	conn.send(b"Content-Type: text/html\r\n")
-	conn.send(b"Connection: close\r\n\r\n")
-	conn.sendall(html.encode())
-	conn.close()
+		conn.send(b"HTTP/1.1 200 OK\r\n")
+		conn.send(b"Content-Type: text/html\r\n")
+		conn.send(b"Connection: close\r\n\r\n")
+		conn.sendall(html.encode())
+		conn.close()
+except KeyboardInterrupt:
+	for pwm in bright:
+		pwm.stop()
+	GPIO.cleanup()
+
+
+
+
