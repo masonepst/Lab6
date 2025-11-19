9e098c068e13235f9bb7c8510d163ebefa32f5e0
masonepst <masonepst@gmail.com>
2025-11-19 16:03:24 -0500

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -115,9 +115,14 @@ if __name__ == '__main__':
 
 
     m1.zero()
-    m2.goAngle(0)
-    m2.goAngle(90)
-    m2.goAngle(-100)
+    m2.zero()
+    m1.goAngle(90)
+    m1.goAngle(-45)
+    m2.goAngle(-90)
+    m2.goAngle(45)
+    m1.goAngle(-135)
+    m1.goAngle(135)
+    m1.goAngle(0)
  
     # While the motors are running in their separate processes, the main
     # code can continue doing its thing: 
