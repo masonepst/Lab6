f23cb855f0b343d9cdea4ae55ba15ebd2ee10bfa
masonepst <masonepst@gmail.com>
2025-12-02 11:53:04 -0500

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -2,29 +2,24 @@ import time
 import multiprocessing
 from shifter import Shifter
 
+
 class Stepper:
     num_steppers = 0
     shifter_outputs = multiprocessing.Value('i', 0)
     seq = [0b0001,0b0011,0b0010,0b0110,0b0100,0b1100,0b1000,0b1001]
     delay = 1200
-    steps_per_degree = 4096/360
+    steps_per_degree = 65536/360
 
     def __init__(self, shifter, lock, newlock):
         self.s = shifter
-        self.angle = multiprocessing.Value('d', 0.0)     # current angle
-        self.step_state = multiprocessing.Value('i', 0)
+        self.angle = multiprocessing.Value('d', 0.0)     # shared angle
+        self.step_state = multiprocessing.Value('i', 0)  # shared step index
+        self.busy = multiprocessing.Value('b', 0)        # 1 if rotating
+
+        self.shifter_bit_start = 4 * Stepper.num_steppers
         self.lock = lock
         self.newlock = newlock
 
-        # Queue for target angles
-        self.target_queue = multiprocessing.Queue()
-
-        # Start the worker process
-        self.process = multiprocessing.Process(target=self._worker)
-        self.process.daemon = True
-        self.process.start()
-
-        self.shifter_bit_start = 4 * Stepper.num_steppers
         Stepper.num_steppers += 1
 
     def __sgn(self, x):
@@ -32,36 +27,56 @@ class Stepper:
             return 0
         return 1 if x > 0 else -1
 
-    def __step(self, dir):
+    def __step(self, dir, angle, step_state):
+        step_state.value = (step_state.value + dir) % 8
+
         with self.newlock:
-            # update step state
-            self.step_state.value = (self.step_state.value + dir) % 8
             mask = ~(0b1111 << self.shifter_bit_start)
             Stepper.shifter_outputs.value &= mask
-            Stepper.shifter_outputs.value |= Stepper.seq[self.step_state.value] << self.shifter_bit_start
+            Stepper.shifter_outputs.value |= Stepper.seq[step_state.value] << self.shifter_bit_start
             self.s.shiftByte(Stepper.shifter_outputs.value)
 
-        # update angle
-        self.angle.value = (self.angle.value + dir / Stepper.steps_per_degree) % 360
+        angle.value = (angle.value + dir / Stepper.steps_per_degree) % 360
 
-    def _worker(self):
-        while True:
-            target_angle = self.target_queue.get()  # blocks until new angle arrives
-            delta = ((target_angle - self.angle.value + 180) % 360) - 180
+    def __rotate(self, delta, angle, step_state, busy_flag):
+        with self.lock:
+            busy_flag.value = 1
+
+            numSteps = int(abs(delta) * Stepper.steps_per_degree)
             direction = self.__sgn(delta)
-            steps = int(abs(delta) * Stepper.steps_per_degree)
-            for _ in range(steps):
-                self.__step(direction)
+
+            for _ in range(numSteps):
+                self.__step(direction, angle, step_state)
                 time.sleep(Stepper.delay / 1e6)
 
+            busy_flag.value = 0
+
+    def rotate(self, delta):
+        if self.busy.value == 1:
+            return
+
+        p = multiprocessing.Process(
+            target=self.__rotate,
+            args=(delta, self.angle, self.step_state, self.busy)
+        )
+        p.start()
+
     def goAngle(self, angle):
-        # push angle into the queue
-        self.target_queue.put(angle)
+        if self.busy.value == 1:
+            return
+
+        diff = angle - self.angle.value
+        move = (diff + 180) % 360 - 180
+
+        p = multiprocessing.Process(
+            target=self.__rotate,
+            args=(move, self.angle, self.step_state, self.busy)
+        )
+        p.start()
 
     def zero(self):
         self.angle.value = 0
         self.step_state.value = 0
-
 
 
 
